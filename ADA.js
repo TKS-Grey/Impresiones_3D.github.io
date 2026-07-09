3f635481b038188b3de151f0929090842405a545
TKS-Grey <junkjnjnjnw@gmail.com>
2026-07-09 09:24:14 -0400

    Delete ADA.js

--- ADA.js
+++
@@ -1,8 +0,0 @@
-const boton = document.getElementById('scrollBtn');
-const destino = document.getElementById('destino');
-
-boton.addEventListener('click', () => {
-  destino.scrollIntoView({ 
-    behavior: 'smooth'
-  });
-});
