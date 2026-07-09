3731701ed0c37b58ad853ee73fa483b66db64a98
TKS-Grey <junkjnjnjnw@gmail.com>
2026-07-09 09:22:25 -0400

    button
    Removed email sending functionality and added smooth scrolling feature.

--- ADA.js
+++ ADA.js
@@ -1,26 +1,8 @@
-const nodemailer = require('nodemailer');
+const boton = document.getElementById('scrollBtn');
+const destino = document.getElementById('destino');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail', // o SMTP de tu servidor
-  auth: {
-    user: 'tuemail@gmail.com',
-    pass: 'tu_contraseña_o_token'
-  }
-});
-
-function enviarCorreoBienvenida(destinatario) {
-  const mailOptions = {
-    from: 'tuemail@gmail.com',
-    to: destinatario,
-    subject: 'Bienvenido a nuestra página',
-    text: 'Gracias por registrarte. ¡Disfruta de nuestros servicios!'
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log('Error al enviar correo:', error);
-    } else {
-      console.log('Correo enviado:', info.response);
-    }
+boton.addEventListener('click', () => {
+  destino.scrollIntoView({ 
+    behavior: 'smooth'
   });
-}
+});
